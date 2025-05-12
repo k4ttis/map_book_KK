40403f67a7dcb4bda78bba67d75d13f678f4d1c6
k4ttis <kondzio3001@gmail.com>
2025-05-12 23:04:44 +0200

    wip: add update user functionality

--- utils/controller.py
+++ utils/controller.py
@@ -7,7 +7,7 @@ def add_user(users_data: list) -> None:
     new_name: str = input('podaj imię nowego znajomego: ')
     new_location: str = input('podaj lokalizację: ')
     new_posts: str = input('podaj liczbę postów: ')
-    users.append({'name': new_name, 'location': new_location, 'posts': new_posts}, )
+    users_data.append({'name': new_name, 'location': new_location, 'posts': new_posts}, )
 
 
 def remove_user(users_data: list) -> None:
